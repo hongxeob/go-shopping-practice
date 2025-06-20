1ba519511044dce72c185b0921a325ed09eda47e
hongxeob <sjun2918@naver.com>
2025-06-20 13:17:38 +0900

    feature: server 구조체 추가

--- server/core/server/config.go
+++ server/core/server/config.go
@@ -4,5 +4,6 @@ type Config struct {
 	Name       string `yaml:"name"`
 	Host       string `yaml:"host"`
 	Port       int    `yaml:"port"`
-	Reflection bool   `yaml:"reflection"`
+	Reflection bool   `yaml:"reflection"` // Enable or disable reflection for the server
+
 }
---
+++ server/services/user/internal/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+import (
+	"github.com/hongxeob/go-shopping-practice/server/core/db"
+	"github.com/hongxeob/go-shopping-practice/server/core/server"
+)
+
+type Config struct {
+	Server server.Config `yaml:"server"`
+	DB     db.Config     `yaml:"db"`
+}
